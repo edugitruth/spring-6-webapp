fa593b49923ecfe5f0890f8b40bd2f699bdb6a11
rmekon <ruthbebele@gmail.com>
2025-01-30 17:22:39 -0500

    .jar to .war

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -2,9 +2,10 @@ package guru.springframework.spring6webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitialzer;
 
 @SpringBootApplication
-public class Spring6WebappApplication {
+public class Spring6WebappApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring6WebappApplication.class, args);
